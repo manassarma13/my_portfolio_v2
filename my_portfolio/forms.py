0d9fb45ae8972b698f59dc3147adaf2e0c888a5a
manassarma13 <manassarma121@gmail.com>
2023-01-17 22:32:02 +0530

    minor css changes

--- my_portfolio/forms.py
+++ my_portfolio/forms.py
@@ -3,12 +3,12 @@ from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired
 
 class add_users(FlaskForm):
-    name=StringField("NAME", validators=[DataRequired()])
-    email=EmailField("EMAIL", validators=[DataRequired()])
-    message=StringField("MESSAGE", validators=[DataRequired()])
+    name=StringField("NAME:", validators=[DataRequired()])
+    email=EmailField("EMAIL:", validators=[DataRequired()])
+    message=StringField("MESSAGE:", validators=[DataRequired()])
     submit=SubmitField("Sumbit")
 
 class Login(FlaskForm):
-    user_name=StringField("NAME", validators=[DataRequired()])
-    password=PasswordField("PASSWORD", validators=[DataRequired()])
+    user_name=StringField("NAME:", validators=[DataRequired()])
+    password=PasswordField("PASSWORD:", validators=[DataRequired()])
     submit=SubmitField("Sumbit")
--- my_portfolio/static/main.js
+++ my_portfolio/static/main.js
@@ -1,43 +1,17 @@
-// //transitions
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 51.486060, lng: -3.173050 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
 
-// window.onload = () => {
-//     const anchors = document.querySelectorAll('a'),
-//         transitions_el = document.querySelector('.transition');
-
-//     setTimeout(() => {
-//         transitions_el.classList.remove('is-active');
-//     },500);
-
-
-//     for(let i=0; i<anchors.length; i++) {
-//         const anchor = anchors[i];
-
-//         anchor.addEventListener('click', (e) => {
-//             e.preventDefault();
-//             let target = e.target.href;
-//             transitions_el.classList.add('is-active');
-
-//             setInterval(() => {
-//                 window.location.href = target;
-//             }, 500);
-//         })
-//     }
-// }
-
-// // Initialize and add the map
-// function initMap() {
-//     // The location of Uluru
-//     const uluru = { lat: 51.486060, lng: -3.173050 };
-//     // The map, centered at Uluru
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 16,
-//         center: uluru,
-//     });
-//     // The marker, positioned at Uluru
-//     const marker = new google.maps.Marker({
-//         position: uluru,
-//         map: map,
-//     });
-// }
-
-// window.initMap = initMap;
+window.initMap = initMap;
